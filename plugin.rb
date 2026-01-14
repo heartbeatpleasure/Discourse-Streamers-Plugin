f862032d65cd076acc20444f95834f73f60de519
heartbeatpleasure <info@heartbeatpleasure.com>
2026-01-15 00:58:46 +0100

    fix json output & config

--- plugin.rb
+++ plugin.rb
@@ -1,33 +1,24 @@
 # frozen_string_literal: true
 
-# name: Discourse-Streamers-Plugin
+# name: discourse-streamers-plugin
 # about: Integrate Icecast audio streams with Discourse users (HeartbeatPleasure)
 # version: 0.1.0
 # authors: Chris
 # url: https://github.com/heartbeatpleasure/Discourse-Streamers-Plugin
 
 module ::Streamers
-  # Gebruik dezelfde naam als hierboven, zodat requires_plugin klopt
-  PLUGIN_NAME = "Discourse-Streamers-Plugin"
+  PLUGIN_NAME = "discourse-streamers-plugin"
 end
 
 require_relative "lib/streamers/engine"
 
 after_initialize do
-  #
-  # Routes toevoegen
-  #
+  # Globale routes toevoegen
   Discourse::Application.routes.append do
-    # Alias: /streams → live streams JSON (en later HTML via frontend)
+    # Alias: /streams → Streamers::StreamsController#index
     get "/streams" => "streamers/streams#index"
 
-    # Mount de engine voor /streamers/*
+    # Engine mount voor /streamers/*
     mount ::Streamers::Engine, at: "/streamers"
   end
-
-  #
-  # BELANGRIJK:
-  # Geen directe calls naar SiteSetting.* hier, zodat bootstrap niet faalt
-  # als settings nog niet zijn geregistreerd.
-  #
 end
